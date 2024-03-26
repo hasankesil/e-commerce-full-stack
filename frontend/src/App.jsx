04cf6572c1e2efe79558288ddaa7fdd045a51c0f
hasankesil <hasankesil@hotmail.com>
2024-03-27 00:59:28 +0300

    Blogs, Brands, and HomePage components added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,17 @@
+import React from 'react'
 import './App.css'
-import Campaigns from './components/Campaigns/Capaigns'
-import Categories from './components/Categories/Categories'
-import Footer from './components/Layout/Footer/Footer'
-import Header from './components/Layout/Header/Header'
-import Policy from './components/Layout/Policy/Policy'
-import Products from './components/Products/Products'
-import Sliders from './components/Slider/Sliders'
+
+import HomePage from './pages/HomePage'
 
 function App() {
 
 
   return (
-    <>
-      <Header />
-      <Sliders/>
-      <Categories/>
-      <Products/>
-      <Campaigns />
-      <Products />
-      <Policy />
-      <Footer />
-    </>
+    <React.Fragment>
+      <HomePage />
+    </React.Fragment>
+
+
   )
 }
 
