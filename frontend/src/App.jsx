c3874941432ca75e15e57a40e56968989d2a264a
hasankesil <hasankesil@hotmail.com>
2024-03-26 23:20:32 +0300

    Category and SliderItem components added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import './App.css'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import Policy from './components/Policy'
+import Categories from './components/Categories/Categories'
+import Footer from './components/Layout/Footer/Footer'
+import Header from './components/Layout/Header/Header'
+import Policy from './components/Layout/Policy/Policy'
+import Sliders from './components/Slider/Sliders'
 
 function App() {
 
@@ -9,6 +11,8 @@ function App() {
   return (
     <>
       <Header />
+      <Sliders/>
+      <Categories/>
       <Policy />
       <Footer />
     </>
---
+++ frontend/src/components/Categories/CategoryItem.jsx
@@ -0,0 +1,14 @@
+import "./CategoryItems.css";
+
+function CategoryItem() {
+    return (
+        <li className="category-item">
+            <a href="#">
+                <img src="img/categories/categories1.png" alt="" className="category-image" />
+                <span className="category-title">Smartphone</span>
+            </a>
+        </li>
+    )
+}
+
+export default CategoryItem
