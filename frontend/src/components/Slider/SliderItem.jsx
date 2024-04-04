957b3bb9d56f32cf6f520c20aa13a67ed2e4b721
hasankesil <hasankesil@hotmail.com>
2024-04-04 21:38:29 +0300

    Slider onclick and state edited

--- frontend/src/components/Slider/SliderItem.jsx
+++ frontend/src/components/Slider/SliderItem.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function SliderItem() {
+function SliderItem({imageSrc}) {
   return (
       <div className="slider-item fade">
           <div className="slider-image">
-              <img src="img/slider/slider1.jpg" className="img-fluid" alt="" />
+              <img src={imageSrc} className="img-fluid" alt="" />
           </div>
           <div className="container">
               <p className="slider-title">SUMMER 2022</p>
--- frontend/src/components/Slider/Sliders.jsx
+++ frontend/src/components/Slider/Sliders.jsx
@@ -1,33 +1,48 @@
+import { useState } from "react";
 import SliderItem from "./SliderItem";
 import "./Sliders.css";
 
 function Sliders() {
+    const [currentSlide, setcurrentSlide] = useState(0);
+
+    const SlideFunctions = {
+        next: () => {
+            setcurrentSlide((prevState) => (prevState + 1) % 3);
+        },
+        prev: () => {
+            setcurrentSlide((prevState) => (prevState - 1 + 3) % 3);
+        }
+    };
+
     return (
         <section className="slider">
             <div className="slider-elements">
-                <SliderItem/>
+                {currentSlide === 0 && <SliderItem imageSrc="img/slider/slider1.jpg" />}
+                {currentSlide === 1 && <SliderItem imageSrc="img/slider/slider2.jpg" />}
+                {currentSlide === 2 && <SliderItem imageSrc="img/slider/slider3.jpg" />}
+
                 <div className="slider-buttons">
-                    <button >
-                        <i className="bi bi-chevron-left"></i>
+                    <button>
+                        <i className="bi bi-chevron-left" onClick={SlideFunctions.prev}></i>
                     </button>
-                    <button >
-                        <i className="bi bi-chevron-right"></i>
+                    <button>
+                        <i className="bi bi-chevron-right" onClick={SlideFunctions.next}></i>
                     </button>
                 </div>
                 <div className="slider-dots">
-                    <button className="slider-dot active" >
+                    <button className={`slider-dot ${currentSlide === 0 ? "active" : ""}`} onClick={()=> setcurrentSlide(0) }>
                         <span></span>
                     </button>
-                    <button className="slider-dot" >
+                    <button className={`slider-dot ${currentSlide === 1 ? "active" : ""}`} onClick={() => setcurrentSlide(1)}>
                         <span></span>
                     </button>
-                    <button className="slider-dot" >
+                    <button className={`slider-dot ${currentSlide === 2 ? "active" : ""}`} onClick={() => setcurrentSlide(2)}>
                         <span></span>
                     </button>
                 </div>
             </div>
         </section>
-    )
+    );
 }
 
-export default Sliders
+export default Sliders;
