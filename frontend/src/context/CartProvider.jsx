b1ab6cb192b6ca96b4780e3ff0836b4387a43d0c
hasankesil <hasankesil@hotmail.com>
2024-04-07 15:46:02 +0300

    Cart localStorage states added

--- frontend/src/context/CartProvider.jsx
+++ frontend/src/context/CartProvider.jsx
@@ -1,11 +1,17 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
 export const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
-    
-    const [cartItems, setCartItems] = useState([]);
+
+    const [cartItems, setCartItems] = useState(localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [] );
+
+    useEffect(() => {
+      
+            localStorage.setItem("cartItems", JSON.stringify(cartItems))
+       
+    }, [cartItems])
 
     console.log("cart items:", cartItems);
 
