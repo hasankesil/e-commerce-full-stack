417f9e391690fea8b5946fc69db8a35075f567e8
hasankesil <hasankesil@hotmail.com>
2024-04-11 00:52:46 +0300

    Category deleted (delete)

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -37,21 +37,16 @@ router.get("/", async (req, res) => {
 
 // Belirli bir kategori getirme (read-single)
 
-router.get("/:categoryId", async (req,res)=> {
-
+router.get("/:categoryId", async (req, res) => {
   try {
-     const categoryId = req.params.categoryId;
-     const category = await Category.findById(categoryId);
-     console.log("kategori başarıyla getirildi:", category)
-       res.status(200).json(category);
-    
+    const categoryId = req.params.categoryId;
+    const category = await Category.findById(categoryId);
+    console.log("kategori başarıyla getirildi:", category);
+    res.status(200).json(category);
   } catch (error) {
     console.error("kategori getirilirken  hata oluştu", error);
-    res.status(500).json({error: "sunucu hatası!!"});
-
+    res.status(500).json({ error: "sunucu hatası!!" });
   }
-
-
 });
 
 // update
@@ -75,6 +70,25 @@ router.put("/:categoryId", async (req, res) => {
   } catch (error) {
     console.error("Kategori güncellenirken bir hata oluştu:", error);
     res.status(500).json({ error: "Sunucu hatası." });
+  }
+});
+
+//delete
+
+router.delete("/:categoryId", async (req, res) => {
+  try {
+    const categoryId = req.params.categoryId;
+
+    const deletedCategory = await Category.findByIdAndDelete(categoryId);
+
+    if (!deletedCategory) {
+      return res.status(404).json({ error: "Category not found." });
+    }
+
+    res.status(200).json(deletedCategory);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
   }
 });
 
