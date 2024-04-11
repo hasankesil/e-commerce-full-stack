0452800c91c05cccfaf51c8336fadc7df732e58a
hasankesil <hasankesil@hotmail.com>
2024-04-11 14:15:31 +0300

    update coupon added

--- backend/routes/coupon.js
+++ backend/routes/coupon.js
@@ -8,6 +8,11 @@ const Coupon = require("../models/Coupon.js");
 // Yeni bir kupon oluşturma (Create)
 router.post("/", async (req, res) => {
     try {
+        const {code} = req.body;
+        const existingCoupon = await Coupon.findOne({code});
+        if (existingCoupon){
+            return res.status(400).json({error: "this coupon is alreacy exists"})
+        }
       const newCoupon = new Coupon(req.body);
       await newCoupon.save();
 
@@ -62,6 +67,29 @@ router.get("/code/:couponCode", async (req, res) => {
     }
     const { discountPercent } = coupon;
     res.status(200).json({ discountPercent });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
+// Kategori güncelleme (Update)
+router.put("/:couponId", async (req, res) => {
+  try {
+    const couponId = req.params.couponId;
+    const updates = req.body;
+
+    const existingCoupon = await Coupon.findById(couponId);
+
+    if (!existingCoupon) {
+      return res.status(404).json({ error: "Coupon not found." });
+    }
+
+    const updatedCoupon = await Coupon.findByIdAndUpdate(couponId, updates, {
+      new: true,
+    });
+
+    res.status(200).json(updatedCoupon);
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "Server error." });
