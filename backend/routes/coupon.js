46352b6ca9cfb434fe80363542f4612a8c0f0231
hasankesil <hasankesil@hotmail.com>
2024-04-11 14:06:38 +0300

    coupon get-all , get-single and get-by-coupon-code added

--- backend/routes/coupon.js
+++ backend/routes/coupon.js
@@ -18,5 +18,58 @@ router.post("/", async (req, res) => {
     }
   });
 
+  //! tüm kuponları, getirme (read-all)
+router.get("/", async (req, res) => {
+  try {
+    const coupons = await Coupon.find();
+    console.log("Kategoriler başarıyla getirildi:", coupons);
+    res.status(200).json(coupons);
+  } catch (error) {
+    console.error("Kategorileri getirirken bir hata oluştu:", error);
+    res.status(500).json({ error: "Sunucu hatası!!" });
+  }
+});
+
+// Belirli bir kuponu getirme (Read - Single by Coupon ID)
+router.get("/:couponId", async (req, res) => {
+  try {
+    const couponId = req.params.couponId;
+
+    const coupon = await Coupon.findById(couponId);
+
+    if (!coupon) {
+      return res.status(404).json({ error: "Coupon not found." });
+    }
+  
+
+    res.status(200).json(coupon);
+ } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
+
+// Belirli bir kuponu getirme (Read - Single by Coupon Code)
+router.get("/code/:couponCode", async (req, res) => {
+  try {
+    const couponCode = req.params.couponCode;
+
+    const coupon = await Coupon.findOne({ code: couponCode });
+
+    if (!coupon) {
+      return res.status(404).json({ error: "Coupon not found." });
+    }
+    const { discountPercent } = coupon;
+    res.status(200).json({ discountPercent });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
+
+
+
 
   module.exports = router
